2deb42b90d921129ead5f28af36368a3acf17a4d
Austen Cote <austencote@Austens-MBP.home>
2020-09-23 10:56:40 -0700

    Updates

---
+++ precache-manifest.3e816df0ffa035d539cd762662274abf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8f6140db59099cf0802ed00588874bf",
+    "url": "/pokemon/index.html"
+  },
+  {
+    "revision": "53d262bbd2eae0167f01",
+    "url": "/pokemon/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2e68be45908571aa1a8c",
+    "url": "/pokemon/static/js/2.3b2c921c.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/pokemon/static/js/2.3b2c921c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53d262bbd2eae0167f01",
+    "url": "/pokemon/static/js/main.1cff01f2.chunk.js"
+  },
+  {
+    "revision": "b931b09f9078cae05f94",
+    "url": "/pokemon/static/js/runtime-main.89aedc36.js"
+  }
+]);
